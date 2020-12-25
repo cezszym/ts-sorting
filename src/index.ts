fadfb7daab80b62051a2ce68d6e3cd93f015f0b2
cezszym <cezaryszymanski7@gmail.com>
2020-12-25 11:23:33 +0100

    app finished - indroduced abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([105, 4, 12, 0, 4]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([105, 4, 12, 0, 4]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('XaDab');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(550);
+linkedList.add(0);
+linkedList.add(-1);
+linkedList.add(3);
+
+linkedList.sort();
+linkedList.print();
